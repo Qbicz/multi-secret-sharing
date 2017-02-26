211c2cdce44e197eb664f2eeb2600ac5e8130392
Qbicz <avenger.v14@gmail.com>
2017-02-26 15:37:16 +0100

    Lambda access structure added

--- cpp/src/main-multi-secret.cpp
+++ cpp/src/main-multi-secret.cpp
@@ -55,7 +55,36 @@ namespace Roy
             output << ")" << endl;
             return output;
         }
-};
+    };
+
+    /*
+     * LambdaStructure is called Lambda in the algorithms in the paper
+     */
+    class LambdaStructure
+    {
+        vector<AccessGroup> accessGroups;
+
+    public:
+        LambdaStructure(vector<AccessGroup> vA)
+            : accessGroups {vA}
+        {}
+
+        ~LambdaStructure()
+        {}
+
+        int getGroupsCount () const { return accessGroups.size(); }
+        const vector<AccessGroup>& getAccessGroups() const { return accessGroups; }
+
+        friend std::ostream& operator<< (std::ostream& output, const LambdaStructure& L)
+        {
+            output << "LambdaStructure\n(" << endl;
+            for (auto group : L.accessGroups)
+                output << group;
+            output << ")" << endl;
+            return output;
+        }
+
+    };
 }
 
 int main(void)
@@ -64,12 +93,17 @@ int main(void)
     Roy::Participant P2(13);
     cout << P1 << P2;
 
+    /* Test AccessGroup - move to unit tests! */
     Roy::AccessGroup A1({P1, P2}); // fully initialize member vector using uniform initialization
     cout << A1;
-
     cout << "A1 group has " << A1.getParticipantsCount() << " participants.\n";
-
     cout << "First participant of A1 is: " << A1.getParticipants()[0];
+
+    /* Test LambdaStructure */
+    Roy::LambdaStructure L1({ A1 });
+    cout << L1;
+    cout << "L1 LambdaStructure has " << L1.getGroupsCount() << " access groups.\n";
+    cout << "First access group in L1 is: " << L1.getAccessGroups()[0];
 
     return 0;
 }
