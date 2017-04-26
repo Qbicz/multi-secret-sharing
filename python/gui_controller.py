b0ce8e14b7c16d624f51afd58f80e5e27aa7c29d
Qbicz <avenger.v14@gmail.com>
2017-04-26 23:44:20 +0200

    embedded algorithm in a simplistic gui

--- python/gui_controller.py
+++ python/gui_controller.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from multisecret_simple_gui import Ui_scada_gui
+from gui.multisecret_simple_gui import Ui_scada_gui
 
+from multisecret.Dealer import Dealer
 import sys
 
 class MultiSecretProgram(Ui_scada_gui):
@@ -9,9 +10,23 @@ class MultiSecretProgram(Ui_scada_gui):
         self.setupUi(window)
 
         self.button_split.clicked.connect(self.split_secret)
+
         
     def split_secret(self):
+        secret = int(self.secret_1.text())
+        
+        prime = 15487469
+
+        # multi secret sharing parameters
+        n_participants = 3
+        access_structures = [[[1,2,3]]]
+        self.dealer = Dealer(prime, n_participants, [secret], access_structures)
+        self.pseudo_shares = self.dealer.split_secrets()
+        
+        # save params: prime, structure, d, x, to file to recreate Dealer later
+        
         self.statusbar.setText('Secret split!')
+    
     
 
 if __name__ == "__main__":
