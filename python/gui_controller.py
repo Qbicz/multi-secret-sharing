9b3e78e7ec8aee629150de733f0d42659e902409
Qbicz <avenger.v14@gmail.com>
2017-04-29 16:53:17 +0200

    dumping secret shares to json file

--- python/gui_controller.py
+++ python/gui_controller.py
@@ -1,31 +1,71 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from gui.multisecret_simple_gui import Ui_scada_gui
+from gui.multisecret_simple_gui import Ui_multisecret_gui
 
 from multisecret.Dealer import Dealer
 import sys
+import json
 
-class MultiSecretProgram(Ui_scada_gui):
+class BytesEncoder(json.JSONEncoder):
+    """ Provide encoding method for writing bytes to JSON """
+    def default(self, byteobject):
+        return byteobject.hex()
+        
+
+class MultiSecretProgram(Ui_multisecret_gui):
     def __init__(self, window):
-        Ui_scada_gui.__init__(self)
+        Ui_multisecret_gui.__init__(self)
         self.setupUi(window)
 
         self.button_split.clicked.connect(self.split_secret)
 
         
     def split_secret(self):
-        secret = int(self.secret_1.text())
+        secret1 = int(self.secret_1.text())
+        secret2 = int(self.secret_2.text())
+        secrets = [secret1, secret2]
         
         prime = 15487469
 
         # multi secret sharing parameters
         n_participants = 3
-        access_structures = [[[1,2,3]]]
-        self.dealer = Dealer(prime, n_participants, [secret], access_structures)
-        self.pseudo_shares = self.dealer.split_secrets()
+        access_structures = [[[1,2,3]], [[1,2]]]
+        dealer = Dealer(prime, n_participants, secrets, access_structures)
+        pseudo_shares = dealer.split_secrets()
         
-        # save params: prime, structure, d, x, to file to recreate Dealer later
+        for secret_shares in pseudo_shares:
+            print(' secret')
+            for group_shares in secret_shares:
+                print('  group')
+                for user_share in group_shares:
+                    print('  user', user_share.hex())
+                    
+                    
+        # save params: prime, structure, ID (user x), to file to recreate Dealer later
+        self.save_pseudo_shares_to_file(pseudo_shares, dealer.p, 'shares_dict.log')
         
         self.statusbar.setText('Secret split!')
+    
+    
+    def save_pseudo_shares_to_file(self, pseudo_shares, prime, filename):
+        
+        data = { 'prime' : prime,
+                 'pseudo_shares' : pseudo_shares,
+                 'id' : 'lol1' }
+        
+        with open(filename, 'w') as file:
+            json.dump(data, file, cls=BytesEncoder)
+        
+        """
+        with open(filename, 'w') as file:
+            for secret_shares in pseudo_shares:
+            print(' secret')
+            for group_shares in secret_shares:
+                print('  group')
+                for user_share in group_shares:
+                    print('  user', user_share.hex())
+                        file.write('\nShare')
+                        file.write(user_share.hex())
+                        """
     
     
 
